aaa0ef7d5169ef93529d6e68197c679bb65bc029
angwoo0503 <angwoo0503@gmail.com>
2023-07-19 15:32:26 +0900

    파일 업로드

---
+++ MyCalculatorLevel1.playground/Contents.swift
@@ -0,0 +1,30 @@
+class Calculator {
+    let firstNum : Int
+    let secondNum : Int
+    let operators : String
+    
+    init(firstNum: Int, operators: String, secondNum: Int) {
+        self.firstNum = firstNum
+        self.operators = operators
+        self.secondNum = secondNum
+    }
+    
+    func calculate() -> Int? {
+        switch operators {
+        case "+" : return firstNum + secondNum
+        case "-" : return firstNum - secondNum
+        case "*" : return firstNum * secondNum
+        case "/" : return firstNum / secondNum
+        default:
+            return nil
+        }
+    }
+}
+
+
+let calculator = Calculator(firstNum: 5, operators: "+", secondNum: 3)
+if let result = calculator.calculate() {
+    print("결과: \(result)")
+} else {
+    print("유효하지 않은 연산이니 다시 한 번 확인해주세요.")
+}
---
+++ MyCalculatorLevel3.playground/Contents.swift
@@ -0,0 +1,82 @@
+class Calculator{
+    let firstNum : Int
+    let secondNum : Int
+    let operators : String
+    
+    init(firstNum: Int, operators: String, secondNum: Int) {
+        self.firstNum = firstNum
+        self.operators = operators
+        self.secondNum = secondNum
+    }
+    
+    func calculate() -> Int? {
+        switch operators {
+        case "+" : return DivideOperation(firstNum: firstNum, operators: "+", secondNum: secondNum).result
+        case "-" : let operation = DivideOperation(firstNum: firstNum, operators: "-", secondNum: secondNum)
+            return operation.result
+        case "*" : let operation = DivideOperation(firstNum: firstNum, operators: "*", secondNum: secondNum)
+            return operation.result
+        case "/" : let operation = DivideOperation(firstNum: firstNum, operators: "/", secondNum: secondNum)
+            return operation.result
+        default:
+            return nil
+        }
+    }
+}
+
+
+
+class AddOperation: Calculator{
+    var result: Int {
+        let result = firstNum + secondNum
+        return result
+    }
+}
+class SubstractOperation: Calculator {
+    var result: Int {
+        let result = firstNum - secondNum
+        return result
+    }
+}
+class MultiplyOperation: Calculator {
+    var result: Int {
+        let result = firstNum * secondNum
+        return result
+    }
+}
+class DivideOperation: Calculator {
+    var result: Int? {
+        guard secondNum != 0 else {
+            return nil
+        }
+        return firstNum / secondNum
+    }
+}
+
+
+
+
+func runCalculator() {
+    let input = readLine()
+    let inputArray = input?.split(separator: "")
+    let firstNum : Int
+    let secondNum : Int
+    let operators : Int
+    Calculator(firstNum: firstNumber, operators: operators, secondNum: secondNum)
+}
+
+//let calculator = Calculator(firstNum: 5, operators: "/", secondNum: 3)
+//if let result = calculator.calculate() {
+//    print("결과: \(result)")
+//} else {
+//    print("유효하지 않은 연산이니 다시 한 번 확인해주세요.")
+//}
+
+
+/*
+ 레벨2와 다른점
+ 각 연산에 대한 책임이 해당 연산을 처리하는 개별 클래스에만 명확하게 할당된다. 각 클래스는 특정 연산에 대한 구체적인 책임을 가지며
+ 변경 사항이 해당 연산에만 영향을 미친다. 결론 : 클래스의 역할과 책임이 명확해지고 코드의 가독성과 이해도가 향상된다.
+ 
+ 예를 들어 DivideOperation 같은 경우 나누게 되는 수인 secondNum이 0일 때는 연산이 불가능하기 때문에 그런 예외를 처리할 때 해당 클래스만 수정하면 되기에 각 연산을 클래스로 나누는 것이 유리하다.
+ */
